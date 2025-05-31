586bcca7961e5c037b10be0c7c6faba7e784d818
bleasey <calvin7ferns@gmail.com>
2025-06-01 00:00:19 +0530

    BPTree changes, removed OG match, added PK attributes

--- src/graphdb.cpp
+++ src/graphdb.cpp
@@ -42,45 +42,6 @@ EdgePK GraphDB::createEdgeByAlias(const std::string &type, const std::string &fr
 	return graph.createEdge(type, from, to, properties);
 }
 
-// check whether nodeB <= nodeA (subset of)
-static bool matchNode(const MatchNode &nodeA, const Node &nodeB)
-{
-	assert(nodeA.edges.size() == 0);
-
-	for (const auto &label : nodeA.labels) {
-		if (not nodeB.labels.contains(label)) {
-			return false;
-		}
-	}
-
-	for (const auto &property : nodeA.properties) {
-		auto it = nodeB.properties.find(property.first);
-		if (it == nodeB.properties.end() or property.second != it->second) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
-MatchResults GraphDB::match(MatchPattern pattern, WhereExp where, MatchResultFmt resultFmt)
-{
-	assert(pattern.numNodes()<2 && "Only single nodes supported");
-	// assert(pattern.isConnected() && "Graph should be connected");
-
-	MatchResults results;
-	const auto &nodeA = pattern.nodes[0];
-	for (const auto &nodeB : graph.nodes) {
-		if (not matchNode(nodeA, nodeB)) {
-			continue;
-		}
-		auto &result = results.emplace_back();
-	    result.createNode(nodeB.labels, nodeB.properties);
-	}
-
-	return results;
-}
-
 void GraphDB::parseCypherScript(const std::string &filename) {
 	Parser parser;
 	parser.parseCypherScript(filename, *this);
